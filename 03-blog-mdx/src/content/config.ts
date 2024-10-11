bb33491e253310c65beb875cc9d31fc3d45b6f7e
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-10-10 23:37:46 -0500

    feat: defining collection references

--- 03-blog-mdx/src/content/config.ts
+++ 03-blog-mdx/src/content/config.ts
@@ -1,19 +1,31 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: ( { image }) => z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: image().refine(img => img.width < 1200, {
-      message: 'Image should be lower than 1200px'
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be lower than 1200px",
+      }),
+      // author: z.string(),
+      author: reference('author'), // reference to authorCollection
+      tags: z.array(z.string()),
     }),
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
