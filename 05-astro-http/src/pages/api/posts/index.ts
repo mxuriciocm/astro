e94d7384dfc5f22f5af94ae864a94794c1d00840
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-10-12 20:06:49 -0500

    feat: implemented GET method to fetch all blog posts using getCollection.

---
+++ 05-astro-http/src/pages/api/posts/index.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const posts = await getCollection("blog");
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: { "Context-Type": "application/json" },
+  });
+};
