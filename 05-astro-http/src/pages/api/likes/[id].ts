aaf414e520053c42b52480c6cfecf2217e3e9157
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-10-15 22:14:07 -0500

    feat: add GET, PUT methods for likes api

--- 05-astro-http/src/pages/api/likes/[id].ts
+++ 05-astro-http/src/pages/api/likes/[id].ts
@@ -1,20 +1,51 @@
 import type { APIRoute } from "astro";
 import { eq, db, Posts, like } from "astro:db";
-export const prerender = false
+export const prerender = false;
 
-export const GET: APIRoute = async ({ params, request }) =>{
+export const GET: APIRoute = async ({ params, request }) => {
+  const { id } = params;
+  const posts = await db.select().from(Posts).where(eq(Posts.id, id!));
 
-    const { id } = params;
-    const posts = await db.select().from(Posts).where(eq(Posts.id, id!))
-    
-    if (posts.length === 0){
-        const post = {
-            id: id,
-            title: 'Post not found',
-            likes: 0
-        }
-        return new Response(JSON.stringify(post), {status: 200, headers: {'Content-Type': 'application/json'}})
-    }
+  if (posts.length === 0) {
+    const post = {
+      id: id,
+      title: "Post not found",
+      likes: 0,
+    };
+    return new Response(JSON.stringify(post), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
-    return new Response(JSON.stringify(posts.at(0)), {status: 200, headers: {'Content-Type': 'application/json'}})
-}
+  return new Response(JSON.stringify(posts.at(0)), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const { id } = params;
+  const posts = await db.select().from(Posts).where(eq(Posts.id, id!));
+  const { likes = 0 } = await request.json();
+  if (posts.length === 0) {
+    const newPost = {
+      id: id!,
+      title: "Post not found",
+      likes: 0,
+    };
+
+    await db.insert(Posts).values(newPost);
+    posts.push(newPost);
+  }
+
+  const post = posts.at(0)!;
+  post.likes = post.likes + likes;
+
+  await db.update(Posts).set(post).where(eq(Posts.id, id!));
+
+  return new Response(JSON.stringify(""), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
