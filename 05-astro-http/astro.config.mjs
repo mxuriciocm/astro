d9559b488b5855955bd978208ba426356c66167e
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2024-10-15 23:28:46 -0500

    feat: add likebutton component using vue and display likes on each post

--- 05-astro-http/astro.config.mjs
+++ 05-astro-http/astro.config.mjs
@@ -6,10 +6,12 @@ import node from "@astrojs/node";
 
 import db from "@astrojs/db";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), vue()],
   output: "hybrid",
   // Modes: 'hybrid' or 'server'
   // 'server' - only static generation
