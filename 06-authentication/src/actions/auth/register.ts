d9ef677b4ebac5af19e669d7d1c0e2b3114c3562
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-10-22 00:04:59 -0500

    feat: add authentication with email and password using firebase

--- 06-authentication/src/actions/auth/register.ts
+++ 06-authentication/src/actions/auth/register.ts
@@ -1,5 +1,7 @@
+import { firebase } from "@/firebase";
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const registerUser = defineAction({
   accept: "form",
@@ -7,19 +9,50 @@ export const registerUser = defineAction({
     name: z.string().min(2),
     email: z.string().email(),
     password: z.string().min(6),
-    remember_me: z.boolean().optional().optional(),
+    remember_me: z.boolean().optional(),
   }),
-  handler: async ({ name, email, password, remember_me }, { cookies }) => {
-    if (remember_me) {
-      cookies.set("email", email, {
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 year
-        path: '/'
+  handler: async (input, context) => {
+    // Cookies
+    if (input.remember_me) {
+      context.cookies.set("email", input.email, {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 año
+        path: "/", // valida en todos los paths
       });
     } else {
-      cookies.delete('email', {
-        path: '/',
-      })
+      context.cookies.delete("email", {
+        path: "/",
+      });
     }
-    return { ok: true, msg: 'User create'};
+
+    // Creacion de usuario
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        firebase.auth,
+        input.email,
+        input.password
+      );
+
+      const user = userCredential.user;
+
+      // Extraer solo los datos necesarios y serializables
+      const userData = {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        emailVerified: user.emailVerified,
+      };
+
+      // Actualizar el nombre
+
+      // Verificar el correo electronico
+
+      return userData;
+    } catch (e) {
+      console.log(e);
+      throw new Error("Something went wrong");
+    }
+
+    return { ok: true, msg: "User created" };
   },
 });
